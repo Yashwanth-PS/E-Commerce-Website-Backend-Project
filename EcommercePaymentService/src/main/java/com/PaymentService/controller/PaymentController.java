143027f6fe7c4fb1b133de2c392d2f4ad8d79afe
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-02-07 20:31:02 +0530

    EcommercePaymentService - Updated the Payment COntroller Created the Initialize Payment API

--- EcommercePaymentService/src/main/java/com/PaymentService/controller/PaymentController.java
+++ EcommercePaymentService/src/main/java/com/PaymentService/controller/PaymentController.java
@@ -1,4 +1,31 @@
 package com.PaymentService.controller;
 
+import com.PaymentService.dto.InitiatePaymentRequestDTO;
+import com.PaymentService.service.PaymentService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
 public class PaymentController {
+
+    private PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/")
+    public String initializePayment(@RequestBody InitiatePaymentRequestDTO paymentRequestDTO) throws Exception {
+        return paymentService.initializePayment(paymentRequestDTO.getOrderId(),
+                paymentRequestDTO.getAmount(),
+                paymentRequestDTO.getPhoneNumber());
+    } //POST: http://localhost:8383/payment/
+    /* {
+        "orderId": "123456",
+        "amount": "1000",
+        "phoneNumber": "9876543210"
+    } */
 }
