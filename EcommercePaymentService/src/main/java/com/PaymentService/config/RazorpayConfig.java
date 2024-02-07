a765bd2b16b9ce67f93fc991c80a6811ecb4f816
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-02-07 20:30:26 +0530

    EcommercePaymentService - Updated the RazorPay Config

--- EcommercePaymentService/src/main/java/com/PaymentService/config/RazorpayConfig.java
+++ EcommercePaymentService/src/main/java/com/PaymentService/config/RazorpayConfig.java
@@ -1,4 +1,21 @@
 package com.PaymentService.config;
 
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RazorpayConfig {
+    @Value("$(razorpay.key.id)")
+    private String razorPayKeyID;
+
+    @Value("$(razorpay.key.secret)")
+    private String razorPayKeySecret;
+    @Bean // Spring Automatically calls the Method and creates a Bean (Object) of the return type
+    public RazorpayClient createRazorpayClient() throws RazorpayException {
+        // Creating an Object of Razor Pay Client - By Passing the API Key and Secret Key
+        return new RazorpayClient(razorPayKeyID, razorPayKeySecret);
+    } // Spring makes sure that there is only one Object of RazorPay Client across the Project
 }
