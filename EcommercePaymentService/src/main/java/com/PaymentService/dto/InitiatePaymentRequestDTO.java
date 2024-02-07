b1b67a66a4fa96d05c15e165ad32f0e22da1608a
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-02-07 20:32:10 +0530

    EcommercePaymentService - Updated the InitiatePaymentRequestDTO

--- EcommercePaymentService/src/main/java/com/PaymentService/dto/InitiatePaymentRequestDTO.java
+++ EcommercePaymentService/src/main/java/com/PaymentService/dto/InitiatePaymentRequestDTO.java
@@ -1,4 +1,12 @@
 package com.PaymentService.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class InitiatePaymentRequestDTO {
+    private Long orderId;
+    private Long amount;
+    private String phoneNumber;
 }
