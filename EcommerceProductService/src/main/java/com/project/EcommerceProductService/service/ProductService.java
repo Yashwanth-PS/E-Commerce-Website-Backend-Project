60870ffaf372da361881f1778ef0c05566070f04
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-03-02 22:47:44 +0530

    EcommerceProductService - Updated Product Service Interface

--- EcommerceProductService/src/main/java/com/project/EcommerceProductService/service/ProductService.java
+++ EcommerceProductService/src/main/java/com/project/EcommerceProductService/service/ProductService.java
@@ -1,16 +1,16 @@
 package com.project.EcommerceProductService.service;
 
+import com.project.EcommerceProductService.dto.GenericProductDTO;
 import com.project.EcommerceProductService.dto.ProductListResponseDTO;
 import com.project.EcommerceProductService.dto.ProductRequestDTO;
-import com.project.EcommerceProductService.dto.ProductResponseDTO;
 import com.project.EcommerceProductService.exception.ProductNotFoundException;
 import com.project.EcommerceProductService.model.Product;
 
 public interface ProductService {
     ProductListResponseDTO getAllProducts();
-    ProductResponseDTO getProductById(int id) throws ProductNotFoundException;
-    ProductResponseDTO createProduct(ProductRequestDTO productRequestDTO);
+    GenericProductDTO getProductById(int id) throws ProductNotFoundException;
+    GenericProductDTO createProduct(ProductRequestDTO productRequestDTO);
     boolean deleteProduct(int id);
     Product updateProduct(int id, Product updateProduct);
-    ProductResponseDTO findProductByTitle(String title);
+    GenericProductDTO findProductByTitle(String title);
 }
