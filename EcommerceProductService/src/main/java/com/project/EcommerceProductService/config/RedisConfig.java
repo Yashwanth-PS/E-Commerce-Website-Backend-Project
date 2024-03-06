476960808ecf041df6d1b289c02a7b32354140a1
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-03-06 23:51:59 +0530

    EcommerceProductService - Added the Redis Config

--- EcommerceProductService/src/main/java/com/project/EcommerceProductService/config/RedisConfig.java
+++ EcommerceProductService/src/main/java/com/project/EcommerceProductService/config/RedisConfig.java
@@ -1,14 +1,15 @@
-package com.project.EcommerceProductService.configuration;
+package com.project.EcommerceProductService.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
-public class RedisConfiguration {
+public class RedisConfig { // Redis Connection Factory, JedisConnectionFactory, lettuce Connection Factory
     @Bean // JedisConnectionFactory - Not supported with some of the Spring Boot Versions in that case use RedisConnectionFactory
-    JedisConnectionFactory jedisConnectionFactory() {
+    public JedisConnectionFactory jedisConnectionFactory() {
         return new JedisConnectionFactory();
     }
 
@@ -16,6 +17,13 @@ public class RedisConfiguration {
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
         return template;
     }
 }
--- EcommerceProductService/src/main/java/com/project/EcommerceProductService/config/RestTemplateConfig.java
+++ EcommerceProductService/src/main/java/com/project/EcommerceProductService/config/RestTemplateConfig.java
@@ -1,8 +1,10 @@
 package com.project.EcommerceProductService.configuration;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
@@ -10,5 +12,11 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplateBuilder restTemplateBuilder() {
         return new RestTemplateBuilder();
+    }
+
+    @Bean // By Default the bean of RestTemplate is created by Spring when we run the application
+    @LoadBalanced // LoadBalanced RestTemplate
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
