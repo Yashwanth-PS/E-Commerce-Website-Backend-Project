8e5ac7203e6f305d0ed39e030f40e4204fcb9b42
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-03-15 21:18:13 +0530

    Email Service - Created SendSMSConsumer Class

---
+++ EmailService/src/main/java/com/emailservice/consumer/SendSMSConsumer.java
@@ -0,0 +1,9 @@
+package com.emailservice.consumer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SendSMSConsumer {
+    private ObjectMapper objectMapper;
+}
