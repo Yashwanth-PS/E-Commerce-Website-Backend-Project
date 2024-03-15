c7813722570e360672047f72bb1c0da5bc3514ef
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-03-15 21:17:49 +0530

    Email Service - Created SendEmailDTO

---
+++ EmailService/src/main/java/com/emailservice/dto/SendEmailDTO.java
@@ -0,0 +1,13 @@
+package com.emailservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SendEmailDTO { // Information that the consumer would require to send an email to the user
+    private String from;
+    private String to;
+    private String subject;
+    private String body;
+}
