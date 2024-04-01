19e60fee6c0ed0d2d8f37e49fcaa7655cc373a43
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-04-01 23:34:35 +0530

    EcommerceProductService - Updated the Global COntroller Advice

--- EcommerceProductService/src/main/java/com/project/EcommerceProductService/controller/controllerAdvice/GlobalControllerAdvice.java
+++ EcommerceProductService/src/main/java/com/project/EcommerceProductService/controller/controllerAdvice/GlobalControllerAdvice.java
@@ -1,20 +1,34 @@
 package com.project.EcommerceProductService.controller.controllerAdvice;
 
-import com.project.EcommerceProductService.dto.ErrorResponseDTO;
+import com.project.EcommerceProductService.dto.ExceptionDTO;
 import com.project.EcommerceProductService.exception.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice // Global Exception Handler
 public class GlobalControllerAdvice {
     // Anywhere in the code if we throw Null Pointer Exception which is not caught it will be handled
     @ExceptionHandler(value = ProductNotFoundException.class)
-    public ResponseEntity<ErrorResponseDTO> handleProductNotFoundException(Exception ex){
-        ErrorResponseDTO errorResponse = new ErrorResponseDTO();
-        errorResponse.setMessage(ex.getMessage());
-        errorResponse.setMessageCode(404);
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    /* @ResponseStatus(HttpStatus.NOT_FOUND)
+    @RequestBody() */
+    public ResponseEntity<ExceptionDTO> handleProductNotFoundException(ProductNotFoundException productNotFoundException){
+        ExceptionDTO exceptionDTO = new ExceptionDTO();
+        exceptionDTO.setHttpStatus(HttpStatus.NOT_FOUND);
+        exceptionDTO.setMessage(productNotFoundException.getMessage());
+        return new ResponseEntity<>(exceptionDTO, HttpStatus.NOT_FOUND);
     }
+
+    /* @ExceptionHandler(ArrayIndexOutOfBoundsException.class)
+    private ResponseEntity<ExceptionDTO> handleArrayIndexOutOfBoundException(ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException) {
+        return null;
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    private ResponseEntity<ExceptionDTO> handleNullPointerException(NullPointerException nullPointerException) {
+        return null;
+    } */
 }
