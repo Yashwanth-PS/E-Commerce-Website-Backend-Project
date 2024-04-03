b4c2397aa688d5277dcf303ee9d3f32d591f6945
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-04-03 23:32:32 +0530

    EcommerceProductService - Updated the Product Repository

--- EcommerceProductService/src/main/java/com/project/EcommerceProductService/repository/ProductRepository.java
+++ EcommerceProductService/src/main/java/com/project/EcommerceProductService/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.project.EcommerceProductService.repository;
 
 import com.project.EcommerceProductService.model.Product;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,9 +12,16 @@ import java.util.UUID;
 @Repository // Create a Bean
 public interface ProductRepository extends JpaRepository<Product, UUID> {
     // Auto-Generated Custom Queries
+    @Override
+    List<Product> findAll(); // Get all the products from the Product table.
     Product findByTitle(String title);
     Product findByTitleAndDescription(String title, String Description); // SELECT * FROM Product WHERE title = "" AND description = ""
     Product findByTitleOrDescription(String title, String Description); // SELECT * FROM Product WHERE title = "" OR description = ""
+
+    @Override
+    <S extends Product> List<S> findAll(Example<S> example);
+
+    List<Product> findAllByPrice_ValueLessThan(Integer x);
 
     /* Product findByPrice_AmountLessThan(double price); // < price
     Product findByPrice_AmountLessThanEqual(double price); // <= price
@@ -27,6 +35,9 @@ public interface ProductRepository extends JpaRepository<Product, UUID> {
 
     @Query(value = CustomQueries.FIND_ALL_PRODUCTS, nativeQuery = true)
     Product findAllProducts(String title, UUID id);
+
+    // @Query(value = "SELECT * FROM product WHERE id = 1", nativeQuery = true)
+    List<Product> findAllByPrice_ValueBetween(Integer x, Integer y);
 
     // SELECT * FROM products WHERE LOWER(title) = "iphone" - Offset and Limit
     List<Product> findAllByTitleContainingIgnoreCase(String title, Pageable pageable);
