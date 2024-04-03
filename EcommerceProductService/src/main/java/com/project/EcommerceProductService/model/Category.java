02b1a545a5933de8ccf428e372514cb7cdbdb4b1
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-04-03 23:31:56 +0530

    EcommerceProductService - Updated the Category Model

--- EcommerceProductService/src/main/java/com/project/EcommerceProductService/model/Category.java
+++ EcommerceProductService/src/main/java/com/project/EcommerceProductService/model/Category.java
@@ -1,9 +1,9 @@
 package com.project.EcommerceProductService.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -11,9 +11,32 @@ import java.util.List;
 @Getter
 @Setter
 @Entity //  We can specify the table name (name = "CATEGORY")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category extends BaseModel{
+    @Column(nullable = false)
     private String categoryName;
-    /* @OneToMany(mappedBy = "category") // The mapped By name should be the same as the variable name in Product class
-    // @JoinColumn(name = "category_id") // Adds a column in Product Table
-    private List<Product> products; */
+    @OneToMany(fetch = FetchType.EAGER ,mappedBy = "category") // The mapped By name should be the same as the variable name in Product class
+    @JoinColumn(name = "category_id") // Adds a column (category_id) in Product Table
+    private List<Product> products;
 }
+
+/* Group
+    - Creator
+    - Admin
+    - Members
+
+Student ----- Batch
+
+    1             1
+Student -------- Batch (current batch) => M:1
+   M                1
+
+   1              M
+Student -------- Batch (previous batch) => M:M
+   M               1
+
+    - current batch
+    - previous batch */
+
+// Category ------ Product
