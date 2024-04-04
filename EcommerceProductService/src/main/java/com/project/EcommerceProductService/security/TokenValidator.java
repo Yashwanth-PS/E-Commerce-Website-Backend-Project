eb85f0d611c0086ae0c9d447092f72f78e9bcd1c
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-04-04 23:28:58 +0530

    EcommerceProductService - Added Token Validator in Security Package

---
+++ EcommerceProductService/src/main/java/com/project/EcommerceProductService/security/TokenValidator.java
@@ -0,0 +1,30 @@
+package com.project.EcommerceProductService.security;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+
+@Component
+public class TokenValidator {
+    private RestTemplateBuilder restTemplateBuilder;
+
+    TokenValidator(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplateBuilder = restTemplateBuilder;
+    }
+
+    /**
+     * This method should call the UserService to validate the token.
+     * If the token is valid then return the corresponding object else
+     * return empty.
+     *
+     * @param token
+     * @return
+     */
+    public Optional<JWTPayLoadDTO> validateToken(String token) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        // Make an HTTP call to UserService to call the validateToken method.
+        return Optional.empty();
+    }
+}
